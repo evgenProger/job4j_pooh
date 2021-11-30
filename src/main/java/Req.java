f459cd79ce27c9e85237b60d35289cf2277b3ef4
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-30 18:35:18 +0500

    Finished Req class.

--- src/main/java/Req.java
+++ src/main/java/Req.java
@@ -32,17 +32,20 @@ public class Req {
         String[] arrName = arr[2].split(" ", 2);
         String httpType = arr[0].trim();
         String poohMode = arr[1];
-        String name = "";
-        String param = "";
+        String name;
+        String param;
         String[] paramArr = arrName[1].split("\\s");
-
-        if (!httpType.equals("topic")) {
+        if (httpType.equals("GET") && poohMode.equals("topic")) {
+            String nameAndParam = arrName[0];
+            String[] nameAndParamArr = nameAndParam.split("/");
+            name = nameAndParamArr[0];
+            param = nameAndParamArr[1];
+        } else {
             name = arrName[0];
             param = paramArr[paramArr.length - 1];
             if (param.equals("*/*")) {
                 param = "";
             }
-
         }
         return new Req(httpType, poohMode, name, param);
     }
