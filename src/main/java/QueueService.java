d13c21ce6db194421ccf20a43da06ee8cd242513
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-12-02 13:09:21 +0500

    PoohServer. Topic and Queue mode.

--- src/main/java/QueueService.java
+++ src/main/java/QueueService.java
@@ -7,12 +7,21 @@ public class QueueService implements Service {
     @Override
     public Resp process(Req req) {
         ConcurrentLinkedQueue<String> concurrentQueue = new ConcurrentLinkedQueue<>();
-
+        Resp resp = null;
         if ("POST".equals(req.getHttpRequestType())) {
-            concurrentQueue.add(req.getSourceName());
-
+            concurrentQueue.add(req.getParam());
+            queue.putIfAbsent(req.getSourceName(), concurrentQueue);
+        } else if ("GET".equals(req.getHttpRequestType())) {
+           String status = null;
+           String text = queue.get(req.getSourceName()).poll();
+           if (text.equals(null)) {
+               status = "404";
+           } else {
+               status = "200";
+           }
+           resp = new Resp(text, status);
 
         }
-        return null;
+        return resp;
     }
 }
--- src/main/java/TopicService.java
+++ src/main/java/TopicService.java
@@ -1,6 +1,45 @@
-public class TopicService implements Service{
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TopicService implements Service {
+    private final ConcurrentHashMap<String, ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> topic
+            = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
-        return null;
+        Resp resp = null;
+        ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map = new ConcurrentHashMap<>();
+        ConcurrentLinkedQueue<String> queue;
+        if ("POST".equals(req.getHttpRequestType())) {
+           map = topic.get(req.getSourceName());
+           for (ConcurrentLinkedQueue<String> q: map.values()) {
+                 q.add(req.getParam());
+           }
+        }
+
+        if ("GET".equals(req.getHttpRequestType())) {
+            if (topic.size() == 0) {
+                queue = new ConcurrentLinkedQueue<>();
+                map.putIfAbsent(req.getParam(), queue);
+                topic.putIfAbsent(req.getSourceName(), map);
+            } else {
+                String status = null;
+
+                if (!topic.get(req.getSourceName()).containsKey(req.getParam())) {
+                    queue = new ConcurrentLinkedQueue<>();
+                    map = topic.get(req.getSourceName());
+                    map.put(req.getParam(), queue);
+                }
+                String text = topic.get(req.getSourceName()).get(req.getParam()).poll();
+                if (text == null) {
+                    text = "";
+                    status = "404";
+                } else {
+                    status = "200";
+                }
+                resp = new Resp(text, status);
+            }
+        }
+        return resp;
     }
 }
